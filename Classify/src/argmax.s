848e543b07dc8a3f1c9165cdab5be1a249e5cb9b
liangmingliang <2902753024@qq.com>
2022-05-30 00:16:21 +0800

    fix bugs about argmax

--- Classify/src/argmax.s
+++ Classify/src/argmax.s
@@ -20,20 +20,20 @@ argmax:
 
 loop_start:
           add t0, x0, x0
-          li t1, 0 # represents the max value in vector
+          li t1, -1 # represents the max value in vector
           li t2, -1 # represents the index of max value in vector
 
 
 loop_continue:
              beq t0, a1, loop_end
-             slli t2, t0, 2
-             add t3, a0, t2
-             lw t4, 0(t3)
-             blt t1, t4, else
+             slli t3, t0, 2
+             add t4, a0, t3
+             lw t5, 0(t4)
+             blt t1, t5, else
              then:
              j end
              else:
-             add t1, t4, x0
+             add t1, t5, x0
              add t2, t0, x0
              end:
              addi t0, t0, 1
@@ -41,8 +41,8 @@ loop_continue:
 
 
 loop_end:
-        li t5, 1
-        blt a1, t5, else1
+        li t6, 1
+        blt a1, t6, else1
         add a0, t2, x0
         j end1
         else1:
