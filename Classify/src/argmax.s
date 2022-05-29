cb51e41679a25edfaf649ca91d398b08284e4c26
liangmingliang <2902753024@qq.com>
2022-05-30 00:05:19 +0800

    finished argmax.s

--- Classify/src/argmax.s
+++ Classify/src/argmax.s
@@ -18,23 +18,37 @@ argmax:
 
     # Prologue
 
-
 loop_start:
-
-
-
-
-
-
+          add t0, x0, x0
+          li t1, 0 # represents the max value in vector
+          li t2, -1 # represents the index of max value in vector
 
 
 loop_continue:
-
+             beq t0, a1, loop_end
+             slli t2, t0, 2
+             add t3, a0, t2
+             lw t4, 0(t3)
+             blt t1, t4, else
+             then:
+             j end
+             else:
+             add t1, t4, x0
+             add t2, t0, x0
+             end:
+             addi t0, t0, 1
+             j loop_continue
 
 
 loop_end:
-    
-
+        li t5, 1
+        blt a1, t5, else1
+        add a0, t2, x0
+        j end1
+        else1:
+        li a3, 7
+        end1:
+       
     # Epilogue
 
 
