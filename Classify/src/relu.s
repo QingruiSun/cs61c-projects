a0492082249aa17c4cccc692fbeb32eaa6fe46ef
liangmingliang <2902753024@qq.com>
2022-05-29 20:51:54 +0800

    finished relu

--- Classify/src/relu.s
+++ Classify/src/relu.s
@@ -14,25 +14,37 @@
 # ==============================================================================
 relu:
     # Prologue
+    
 
 
 loop_start:
-    
-
-
-
-
-
-
-
+          add t0, x0, x0
+               
 loop_continue:
-
-
+             beq t0, a1, loop_end
+             slli t1, t0, 2
+             add t2, a0, t1
+             lw t3, 0(t2)
+             blt t3, x0, else
+             then:
+             j end
+             else:
+             add t3, x0, x0
+             j end
+             end:
+             sw t3, 0(t2)
+             addi t0, t0, 1
+             
 
 loop_end:
-
-
+        li, t0, 1
+        blt a1, t0, else1
+        then1:
+        addi a0, x0, 0
+        j end1
+        else1:
+        addi a0, x0, 8
     # Epilogue
 
-    
+        end1:
 	ret
