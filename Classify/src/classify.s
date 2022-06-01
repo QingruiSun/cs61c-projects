bffd14ed69bcacb49308ec1a20b4cbc67264ecd8
liangmingliang <2902753024@qq.com>
2022-06-02 01:14:51 +0800

    fix bugs

--- Classify/src/classify.s
+++ Classify/src/classify.s
@@ -1,5 +1,10 @@
 .globl classify
 
+.data
+m0: .word 0 0
+m1: .word 0 0
+input: .word 0 0
+
 .text
 classify:
     # =====================================
@@ -19,7 +24,11 @@ classify:
     #
     # Usage:
     #   main.s -m -1 <M0_PATH> <M1_PATH> <INPUT_PATH> <OUTPUT_PATH>
-    addi sp sp -76
+    
+    li t0 5
+    bne a0 t0 arg_error
+
+    addi sp sp -52
     sw s0 0(sp)
     sw s1 4(sp)
     sw s2 8(sp)
@@ -37,9 +46,6 @@ classify:
     mv s0 a0 # s0 represents argc
     mv s1 a1 # s1 represents argv
     mv s2 a2
-    
-    li t0 5
-    bne s0 t0 arg_error
 
     # =====================================
     # LOAD MATRICES
@@ -48,47 +54,53 @@ classify:
     # Load pretrained m0
     lw s3 4(s1) # s3 represents path to m0
     mv a0 s3
-    addi a1 sp 52
-    addi a2 sp 56
+    la t0 m0
+    addi a1 t0 0
+    addi a2 t0 4
     jal ra read_matrix
     mv s3 a0 # s3 represents pointer to m0
 
     # Load pretrained m1
     lw s4 8(s1) # s4 represents path to m1
     mv a0 s4
-    addi a1 sp 60
-    addi a2 sp 64
+    la t0 m1
+    addi a1 t0 0
+    addi a2 t0 4
     jal ra read_matrix
     mv s4 a0 # s4 represents pointer to m0
 
     # Load input matrix
     lw s5 12(s1) # s5 represents path to input matrix
     mv a0 s5
-    addi a1 sp 68
-    addi a2 sp 72
+    la t0 input
+    addi a1 t0 0
+    addi a2 t0 4
     jal ra read_matrix
     mv s5 a0 # s5 represents pointer to input matrix
-
-
+    
     # =====================================
     # RUN LAYERS
     # =====================================
     # 1. LINEAR LAYER:    m0 * input
     # 2. NONLINEAR LAYER: ReLU(m0 * input)
     # 3. LINEAR LAYER:    m1 * ReLU(m0 * input)
-    lw t0 52(sp)
-    lw t1 72(sp)
+    la t0 m0
+    lw t0 0(t0)
+    la t1 input
+    lw t1 4(t1)
     mul s6 t0 t1 # s6 represents m0 * input size
     mv a0 s6  
     slli a0 a0 2
     jal ra malloc
     mv s7 a0 # s7 represents m0 * input
     mv a0 s3
-    lw a1 52(sp)
-    lw a2 56(sp)
+    la t0 m0
+    lw a1 0(t0)
+    lw a2 4(t0)
     mv a3 s5
-    lw a4 68(sp)
-    lw a5 72(sp)
+    la t0 input
+    lw a4 0(t0)
+    lw a5 4(t0)
     mv a6 s7
     jal ra matmul 
      
@@ -96,18 +108,23 @@ classify:
     mv a1 s6
     jal ra relu
     
-    lw t0 60(sp)
-    lw t1 72(sp)
+    la t0 m1
+    lw t0 0(t0)
+    la t1 input
+    lw t1 4(t1)
     mul a0 t0 t1
     slli a0 a0 2
     jal ra malloc
     mv s8 a0 # reprents matrix pointer to m1 * ReLU(m0 * input)
     mv a0 s4
-    lw a1 60(sp)
-    lw a2 64(sp)
+    la t0 m1
+    lw a1 0(t0)
+    lw a2 4(t0)
     mv a3 s7
-    lw a4 52(sp)
-    lw a5 72(sp)
+    la t0 m0
+    la t1 input
+    lw a4 0(t0)
+    lw a5 4(t1)
     mv a6 s8
     jal ra matmul
     
@@ -126,8 +143,10 @@ classify:
     # Write output matrix
     lw a0 16(s1)
     mv a1 s8
-    lw a2 60(sp)
-    lw a3 72(sp)
+    la t0 m1
+    la t1 input
+    lw a2 0(t0)
+    lw a3 4(t1)
     jal ra write_matrix
 
     # =====================================
@@ -135,9 +154,11 @@ classify:
     # =====================================
     # Call argmax
     mv a0 s8
-    lw t0 60(sp)
-    lw t1 72(sp)
-    mul a2 t0 t1
+    la t0 m1
+    lw t0 0(t0)
+    la t1 input
+    lw t1 4(t1)
+    mul a1 t0 t1
     jal ra argmax
     mv s9 a0 # s9 represents class index
     mv a0 s8
@@ -167,9 +188,9 @@ classify:
     lw s10 40(sp)
     lw s11 44(sp)
     lw ra 48(sp)
-    addi sp sp 76
+    addi sp sp 52
     ret
 
-    arg_error:
+ arg_error:
     li a1 49
-    jal ra exit2
+    ret
